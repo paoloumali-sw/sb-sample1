2237ef4365e28b64f851bb0aebb3a76f1d6fa40a
Paolo Umali <paolo.umali@solarwinds.cloud>
2019-05-16 05:59:56 +0800

    renamed logger

--- sample/src/main/java/hello/HelloController.java
+++ sample/src/main/java/hello/HelloController.java
@@ -8,6 +8,12 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
+
+        // get a logger instance named com.foo.Bar
+        Logger barlogger = Logger.getLogger(HelloController.class);
+        barlogger.warn("Low fuel level.");
+        barlogger.debug("Starting search for nearest gas station.");
+
         return "Greetings from Spring Boot!";
     }
     
